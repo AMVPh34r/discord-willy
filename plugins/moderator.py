3da3436110af4ebf8038fa5037bcdb60ceafa554
AMV_Ph34r <AMVPh34r@gmail.com>
2016-09-17 21:31:58 -0400

    Add fancy name

--- plugins/moderator.py
+++ plugins/moderator.py
@@ -10,6 +10,7 @@ logs = logging.getLogger("discord")
 
 
 class Moderator(Plugin):
+    fancy_name = "Moderator Commands"
     @staticmethod
     async def get_commands(server):
         commands = [
